0e378f36ce6e2e53a245d559d2ff88bd67d1f5f2
Rowan Speckman <111914117+RSpeckles@users.noreply.github.com>
2023-09-18 12:04:56 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pyautogui
+import solver
+
+driver = webdriver.Chrome()
+url = 'https://www.nytimes.com/games/wordle/index.html'
+driver.get(url)
+
+try:
+    login_form = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/button")
+    login_form.click()
+    time.sleep(1)
+    second_button = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div/div[2]/button[2]")
+    second_button.click()
+    time.sleep(1)
+
+    third_button = driver.find_element(By.XPATH, "/html/body/div/div/dialog/div/button")
+    third_button.click()
+    
+    driver.execute_script("window.scrollBy(0, 200)")
+    time.sleep(2)
+
+    indices = "nnnnn"
+    word = "adieu"
+
+    wordleSolver = solver.Solver()
+
+    while indices != "ccccc":
+
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+
+        time.sleep(5)
+
+        absent_tiles = driver.find_elements(By.CSS_SELECTOR, ".Tile-module_tile__UWEHN[data-state=absent]")
+        present_tiles = driver.find_elements(By.CSS_SELECTOR, ".Tile-module_tile__UWEHN[data-state=present]")
+        correct_tiles = driver.find_elements(By.CSS_SELECTOR, ".Tile-module_tile__UWEHN[data-state=correct]")
+        cAndp = []
+        #print("Absent Tiles:")
+        for tile in absent_tiles:
+            letter = tile.get_attribute("data-letter")
+            tile_text = tile.text.lower()
+            #print(f"Letter: {letter}, Text: {tile_text}")
+
+            position = word.find(tile_text) + 1
+            #print(f"Position in Word: {position}")
+
+            if position != 0:
+                if tile_text in cAndp:
+                    indices = indices[:position - 1] + "p" + indices[position:]
+                else:
+                    indices = indices[:position - 1] + "n" + indices[position:]
+
+        #print("Present Tiles:")
+        for tile in present_tiles:
+            letter = tile.get_attribute("data-letter")
+            tile_text = tile.text.lower()
+            #print(f"Letter: {letter}, Text: {tile_text}")
+
+            position = word.find(tile_text) + 1
+            #print(f"Position in Word: {position}")
+
+            if position != 0:
+                indices = indices[:position - 1] + "p" + indices[position:]
+                cAndp.append(tile_text)
+
+
+        #print("Correct Tiles:")
+        for tile in correct_tiles:
+            letter = tile.get_attribute("data-letter")
+            tile_text = tile.text.lower()
+
+            # print(f"Letter: {letter}, Text: {tile_text}")
+
+            position = word.find(tile_text) + 1
+            #print(f"Position in Word: {position}")
+
+            if position != 0:
+                indices = indices[:position - 1] + "c" + indices[position:]
+                cAndp.append(tile_text)
+
+        print(f"Indices: {indices}")
+
+        word = wordleSolver.solve(indices)
+
+    input("Press Enter to quit the browser...")
+except KeyboardInterrupt:
+    pass
+finally:
+    driver.quit()
---
+++ solver.py
@@ -0,0 +1,69 @@
+import random
+
+class Solver:
+    def __init__(self):
+        f = open("5LetterDict.txt", "r")
+        self.possibleWords = []
+        count = 0
+        self.word = "adieu"
+        while True:
+                line = f.readline()
+                if not line:
+                    break
+                self.possibleWords.append(line)
+                self.possibleWords[-1] = self.possibleWords[-1][:5]
+        
+    def solve(self, letters):
+        if not letters:
+            return "adieu"
+        else:
+            i = 0
+            print("WORD: ", self.word)
+            while i < len(self.possibleWords):
+                flag = False
+                for j in range(len(letters)):
+                    print(j)
+                    if (i >= len(self.possibleWords)):
+                        break
+                    elif letters[j] == "c" and self.possibleWords[i][j] != self.word[j]:
+                        print(self.word[j], " NOT IN PLACE FOR ", self.possibleWords[i])
+                        self.possibleWords.pop(i)
+                        flag = True
+                        break
+                    elif letters[j] == "p" and self.word[j] not in self.possibleWords[i]:
+                        print(self.word[j], " NOT IN ", self.possibleWords[i])
+                        self.possibleWords.pop(i)
+                        flag = True
+                        break
+                    elif letters[j] == "n" and self.word[j] in self.possibleWords[i]:
+                        print(self.word[j], " IS IN ", self.possibleWords[i])
+                        self.possibleWords.pop(i)
+                        flag = True
+                        break
+                if flag == False:
+                    i += 1
+                        
+
+                # flag = False
+                # for j in letters["Green"]:
+                #     if self.possibleWords[i][j[1]] != j[0]:
+                #         self.possibleWords.pop(i)
+                #         flag = True
+                #         continue
+                # if flag == True:
+                #     continue
+                # for j in letters["Red"]:
+                #     if j in self.possibleWords[i]:
+                #         self.possibleWords.pop(i)
+                #         flag = True
+                #         continue
+                # if flag == True:
+                #     continue
+                # for j in letters["Yellow"]:
+                #     if j not in self.possibleWords[i]:
+                #         self.possibleWords.pop(i)
+                #         continue
+            ahsd = random.randint(0,len(self.possibleWords))
+            self.word = "koala"
+            return self.word
+                
